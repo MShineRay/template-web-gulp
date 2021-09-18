157ddd90fcc866dacdacd952fa21a6c754aa8856
msr <mshineray@qq.com>
2021-09-18 18:02:49 +0800

    upd: ++

--- src/static/js/require.config.js
+++ src/static/js/require.config.js
@@ -2,23 +2,23 @@ require.config({
   baseUrl: '/static',
   urlArgs: 'bust=' + (new Date()).getTime(),
   paths: {
-    style: 'assets/require/css.min',
+    style: 'assets/require/css',
     jquery: 'assets/jquery/jquery-1.11.3.min',
-    template: 'assets/artTemplate/template',
+    // template: 'assets/artTemplate/template',
     layui: 'assets/layui/layui',
     mock: 'assets/mockjs/mock',
     index: 'js/index',
     utils: 'js/utils',
   },
   shim: {
-    layui: {
-      deps: ['style!assets/layui/css/layui.css']
-    },
-    index: {
-      deps: ['style!css/index.css']
-    },
-    footer: {
-      deps: ['style!css/footer.css']
-    }
+    // layui: {
+    //   deps: ['style!assets/layui/css/layui.css']
+    // },
+    // index: {
+    //   deps: ['style!assets/styles/index.css']
+    // },
+    // footer: {
+    //   deps: ['style!assets/styles/footer.css']
+    // }
   }
 })
